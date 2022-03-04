d0f47e603d91170a2f2452e5fc7dcfdb9bfaa3b6
SURIYA015 <suriyaswamy18@gmail.com>
2022-03-04 12:21:28 +0530

    domain setup

--- textutils/settings.py
+++ textutils/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p8&svi=!x-+bywym4z95$h)_dt9qw8ut1j0f2=5(7j7h*7han)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','analysetext.herokuapp.com']
 
 
 # Application definition
